2c6065e5ec8ad170ffdb4e6d0855a164fad2587f
xsanket <xsanketkamble232@gmail.com>
2024-07-19 18:09:43 +0530

    "cors"

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ import graphProject from './routes/graphProject.js';
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
