1bbf5ceb78e3e850524324622acdb28ef4d2ab6b
xsanket <xsanketkamble232@gmail.com>
2024-07-19 18:49:34 +0530

    "index.js change"

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://venturehorizon.vercel.app/"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
